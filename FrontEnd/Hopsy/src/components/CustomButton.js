a4967a25d064e35a4e0ef92cabf440eff16c4fc0
Nick Baggett <43483627+nbaggett@users.noreply.github.com>
2019-11-07 18:54:30 -0800

    Preference page (#62)
    * preference page
    
    * updated for android compatability
    
    * wrapped everything in a scroll view
    
    * made some simple ui changes
    
    * fixed the weird dropdown list issue, added some components, a button yeet, made the colors mor uniform, its way better now trust me
    
    * Delete .DS_Store
    
    * various qol changes
    
    * made images png and made references local
    
    * made the header a reusable component
    
    * better header
    
    * fixed expo weirdness
    
    * fixed some issues leftover from merging the code
    
    * made a preference screen in app.js
    
    * bug fixes and folder management
    
    * updated the header component to have better CSS
    
    * header fix
    
    * implemented the preference page into the login screen by adding preference page to the stack navigator

---
+++ FrontEnd/Hopsy/src/components/CustomButton.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+class customButton extends Component {
+  render() {
+    const { text, onPress } = this.props;
+    return (
+      <TouchableOpacity style={styles.buttonStyle} onPress={() => onPress()}>
+        <Text style={styles.textStyle}>{text}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+customButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+    color: "#ffffff",
+    textAlign: "center"
+  },
+
+  buttonStyle: {
+    padding: 10,
+    backgroundColor: "#276612",
+    borderRadius: 15
+  }
+});
+
+export default customButton;
