f530551257359664152efd5ea9222c9b67cdc577
Nick <nick.baggett@yahoo.com>
2019-11-14 13:23:38 -0800

    made some UI unification changes such as making touchable opacity blocks into custom button components, created a check for the login button to prevent crashing if no user data was input, and uploaded a much higher resolution logo image as well as made it square

--- FrontEnd/Hopsy/src/screens/AgeVerificationScreen.js
+++ FrontEnd/Hopsy/src/screens/AgeVerificationScreen.js
@@ -1,126 +1,110 @@
-import React, { Component } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import {
-    View,
-    Text,
-    TouchableOpacity,
-    Image,
-    StatusBar
-} from 'react-native';
+import React, { Component } from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, Image, StatusBar } from "react-native";
 
+import CustomButton from "../components/CustomButton";
 
 class AgeVerificationScreen extends Component {
-    render() {
-        return (
-            <View>
-                <StatusBar
-                    barStyle="dark-content"
-                />
-                <View style={styles.logoContainer}>
-                    <Image 
-                    style={styles.logo}
-                    source={require('../images/Bunny.png')}></Image>
-                    <Text style={styles.title}>Hopsy</Text>
-                    <Text style={styles.subtitle}>A simpler way to enjoy beer.</Text>
-                    <Text style={styles.bigText}> 
-                        Are you over 21? 
-                    </Text>
-                </View>
-                <View style={styles.twinContainer}>
-                    <TouchableOpacity 
-                        style={styles.simpleYesButton}
-                        onPress={() => this.props.navigation.navigate('Login')}>
-                        <Text style={styles.loginText}>Yes</Text>
-                    </TouchableOpacity>
+  render() {
+    return (
+      <View>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../images/Bunny.png")}
+          ></Image>
+          <Text style={styles.title}>Hopsy</Text>
+          <Text style={styles.subtitle}>A simpler way to enjoy beer.</Text>
+          <Text style={styles.bigText}>Are you over 21?</Text>
+        </View>
+        <View style={styles.twinContainer}>
+          <CustomButton
+            style={styles.simpleYesButton}
+            onPress={() => this.props.navigation.navigate("Login")}
+            text="Yes"
+          />
+          <CustomButton
+            style={styles.simpleNoButton}
+            onPress={() => this.props.navigation.navigate("TooYoung")}
+            text="No"
+          />
+        </View>
+      </View>
+    );
+  }
+}
 
-                    <TouchableOpacity 
-                        style={styles.simpleNoButton}
-                        onPress={() => this.props.navigation.navigate('TooYoung')}>
-                        <Text style={styles.loginText}>No</Text>
-                    </TouchableOpacity>            
-                </View>
-            </View>
-        );
-    }
-} 
-
-AgeVerificationScreen.navigationOptions = 
-{
-    header: null
+AgeVerificationScreen.navigationOptions = {
+  header: null
 };
 
 export default AgeVerificationScreen;
 
-  
-const styles = StyleSheet.create(
-{
-    container: 
-    {
-        flex: 1,
-        backgroundColor: '#FFF',
-    },
-    bigText: {
-        fontSize: 45,
-        marginTop: 150,
-        marginBottom: 5,
-        fontWeight: '700',
-        textAlign: 'center',
-    },
-    twinContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        margin: 50,
-    },
-    simpleYesButton: {
-        height: 55,
-        marginTop: 10,
-        width: 120,
-        textAlign: 'center',
-        backgroundColor: 'rgba(rgba(68, 126, 36, 1))',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    simpleNoButton: {
-        height: 55,
-        width: 120,
-        backgroundColor: 'rgba(rgba(68, 126, 36, 1))',
-        textAlign: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 10
-    },
-    loginText:{
-        color:'#fff',
-        flexDirection: 'row',
-        fontSize: 25,
-        paddingLeft : 20,
-        paddingRight : 20
-    },
-    logo: {
-        width: 300,
-        height: 300,
-    },
-    logoContainer: {
-        marginTop: 110,
-        alignItems : 'center',
-        flexGrow: 1,
-        justifyContent: 'center'
-    },
-    title: {
-        fontSize: 45,
-        color: '#000',
-        marginTop: 10,
-        width: 200,
-        textAlign: 'center', 
-        fontWeight: '700'
-    },
-    subtitle: {
-        fontSize: 25,
-        color: '#000',
-        marginTop: 5,
-        width: 500,
-        textAlign: 'center',
-        fontWeight: '600'
-    } 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+  bigText: {
+    fontSize: 45,
+    marginTop: 150,
+    marginBottom: 5,
+    fontWeight: "700",
+    textAlign: "center"
+  },
+  twinContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 50
+  },
+  simpleYesButton: {
+    height: 55,
+    marginTop: 10,
+    width: 120,
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  simpleNoButton: {
+    height: 55,
+    width: 120,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10
+  },
+  loginText: {
+    color: "#fff",
+    flexDirection: "row",
+    fontSize: 25,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  logo: {
+    width: 300,
+    height: 300
+  },
+  logoContainer: {
+    marginTop: 110,
+    alignItems: "center",
+    flexGrow: 1,
+    justifyContent: "center"
+  },
+  title: {
+    fontSize: 45,
+    color: "#000",
+    marginTop: 10,
+    width: 200,
+    textAlign: "center",
+    fontWeight: "700"
+  },
+  subtitle: {
+    fontSize: 25,
+    color: "#000",
+    marginTop: 5,
+    width: 500,
+    textAlign: "center",
+    fontWeight: "600"
+  }
 });
-
