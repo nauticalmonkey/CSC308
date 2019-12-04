44e92c519708c12710a07cf6df9f35ce971f4609
Justin Evans <17054595+nauticalmonkey@users.noreply.github.com>
2019-12-04 00:16:29 -0800

    alaign to center and bigger font

--- FrontEnd/Brewerweb/js/beerRepo.js
+++ FrontEnd/Brewerweb/js/beerRepo.js
@@ -173,9 +173,11 @@ function BeersToList(item, index) {
     var text = document.createTextNode(item.name);
     imgcomponet.src = item.URL;
     imgcomponet.id = "foo" + index;
+    paragraph.style.fontSize = 25;
     paragraph.appendChild(text);
     liElement.appendChild(paragraph)
     liElement.appendChild(imgcomponet);
+    liElement.style.textAlign = "center";
     list.appendChild(liElement);
 
 }
