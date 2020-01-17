7c9571f8ff263e3b1b0ac4dc8b9dd85187a923e6
Nick <nick.baggett@yahoo.com>
2020-01-17 15:11:08 -0800

    more testing

--- FrontEnd/Hopsy/src/__tests__/App-test.js
+++ FrontEnd/Hopsy/src/__tests__/App-test.js
@@ -1,9 +1,24 @@
 import React from "react";
 import Header from "../components/Header";
+import CustomButton from "../components/CustomButton";
 
 import renderer from "react-test-renderer";
 
 test("renders correctly", () => {
   const tree = renderer.create(<Header />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("renders correctly", () => {
+  const tree = renderer
+    .create(
+      <CustomButton
+        text="Test"
+        onPress={() => {
+          alert("Test");
+        }}
+      />
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
