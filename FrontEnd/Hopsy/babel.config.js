94d5a79265315d7d616c68199d53664d7f0addbf
ntreese <43485169+ntreese@users.noreply.github.com>
2020-01-27 14:13:32 -0800

    Update babel.config.js

--- FrontEnd/Hopsy/babel.config.js
+++ FrontEnd/Hopsy/babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ["@babel/preset-expo","module:react-native-dotenv"],
-    plugins: ["@babel/plugin-transform-flow-strip-types","@babel/plugin-proposal-class-properties"]
+    plugins: ["@babel/plugin-proposal-class-properties"]
   };
 };
