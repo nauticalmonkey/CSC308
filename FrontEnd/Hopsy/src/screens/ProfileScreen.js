f181f8ec70c6d99754dd1c2081b04cd92c7b5206
ntreese <ntreese@calpoly.edu>
2020-02-07 15:55:48 -0800

    Meme

--- FrontEnd/Hopsy/src/screens/ProfileScreen.js
+++ FrontEnd/Hopsy/src/screens/ProfileScreen.js
@@ -5,12 +5,46 @@ import Header from "../components/Header";
 import { DrawerActions } from "react-navigation-drawer";
 import Icon from "react-native-vector-icons/Ionicons";
 import HorizontalList from "../components/ProfilePage/HorizontalList";
+import GLOBAL from '../../global';
 
 export default class ProfileScreen extends React.Component {
+  state = {
+    fullname: "No",
+  };
+  
   static navigationOptiosn = {
-    drawerLabel : "Profile",
+    drawerLabel : null,
     drawerIcon : (<Icon name="ios-contact" color={"rgba(68, 126, 36, 1)"} size={10} />)
   }
+
+  _fetchData() {
+    return fetch('http://localhost:8080/GetUserProfile?',
+    {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: GLOBAL.user,
+        })
+    })  
+    .then(response => {
+      this.setState({
+        fullname: response.text(),
+      }, function() {
+      })
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+
+  }
+
+  componentDidMount() {
+    this._fetchData();
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
@@ -20,11 +54,33 @@ export default class ProfileScreen extends React.Component {
         }}/>
         <View style={styles.profileHeader}>
           <Icon name="ios-contact" color={"rgba(68, 126, 36, 1)"} size={150} />
-          <Text style={styles.profileName}>Robert Middleton</Text>
+          {/*<Text style={styles.profileName}>fullname</Text>*/}
+          <Text>{this.state.fullname}</Text>
         </View>
-          <View style={{ flex: 1, marginTop: -300}}>
+
+          <View style={{ flex: 1, marginTop: 10}}>
             <Text style={{ fontSize: 24,  paddingHorizontal: 20 }}>
                 Your top 5 favorite beers!
+            </Text>
+            <View style={styles.listContainer}>
+              <ScrollView horizontal={true}
+                  showsHorizontalScrollIndicator={true}>
+                <HorizontalList imageUri={require('../images/Bunny.png')}
+                    name="Hopsy"/>
+                <HorizontalList imageUri={require('../images/Bunny.png')}
+                    name="Hopsy"/>
+                <HorizontalList imageUri={require('../images/Bunny.png')}
+                    name="Hopsy"/>
+                <HorizontalList imageUri={require('../images/Bunny.png')}
+                    name="Hopsy"/>
+                <HorizontalList imageUri={require('../images/Bunny.png')}
+                    name="Hopsy"/>
+              </ScrollView>
+            </View>
+          </View>
+          <View style={{ flex: 1, marginTop: 10}}>
+            <Text style={{ fontSize: 24,  paddingHorizontal: 20 }}>
+                Your top 5 favorite breweries!
             </Text>
             <View style={styles.listContainer}>
               <ScrollView horizontal={true}
