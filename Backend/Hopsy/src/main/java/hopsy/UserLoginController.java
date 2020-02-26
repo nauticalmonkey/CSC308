cd5680a2d09b854ce1cdbd4b7cebdb9898c9d910
Collin Leslie <>
2020-02-25 17:25:29 -0800

    Cleaned up commented out code and unused imports.

--- Backend/Hopsy/src/main/java/hopsy/UserLoginController.java
+++ Backend/Hopsy/src/main/java/hopsy/UserLoginController.java
@@ -4,14 +4,9 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.json.HTTP;
 import org.json.JSONObject;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,8 +23,6 @@ public class UserLoginController {
 
     Document doc = DBUtils.findDoc(dbCollection, "email", jsObj.get("name").toString());
 
-    // return new ResponseEntity(HttpStatus.OK);
-
     return (doc != null && doc.getString("password").equals(jsObj.get("password")));
   }
 
@@ -45,7 +38,6 @@ public class UserLoginController {
 
     Document doc = DBUtils.findDoc(dbCollection, "email", jsObj.get("username").toString());
 
-    // return new ResponseEntity(HttpStatus.OK);
     System.out.println(doc.getString("fullname"));
     System.out.println("Returning: " + doc.getString("fullname"));
     return doc.getString("fullname");
