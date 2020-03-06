21a23d5c21465ce8e38a35a3286d4558aea52699
Robert Middleton <robert@Roberts-MacBook-Pro-2.local>
2020-03-06 14:16:47 -0800

    before m pull

--- Backend/Hopsy/src/main/java/hopsy/BeerDBController.java
+++ Backend/Hopsy/src/main/java/hopsy/BeerDBController.java
@@ -1,9 +1,14 @@
 package hopsy;
 
+import java.util.Iterator;
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+
 import org.bson.Document;
+import org.json.JSONArray;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +36,18 @@ public class BeerDBController {
         MongoCollection<Document> dbCollection = db.getCollection("beers");
         System.out.println("gimme data");
         Document doc = dbCollection.find().first();
+        // JSONArray ja = (JSONArray) doc.get("beers");
+        // Iterator itr2 = ja.iterator(); 
+        // while (itr2.hasNext())  
+        // { 
+        //     itr1 = ((Map) itr2.next()).entrySet().iterator(); 
+        //     while (itr1.hasNext()) { 
+        //         Map.Entry pair = itr1.next(); 
+        //         System.out.println(pair.getKey() + " : " + pair.getValue()); 
+        //     } 
+        // }
         
+        System.out.println(JSON.parse(doc.getString("beers")));
         return doc.getString("beers");
     }
 }
--- Backend/Hopsy/src/main/java/hopsy/UserLoginController.java
+++ Backend/Hopsy/src/main/java/hopsy/UserLoginController.java
@@ -41,8 +41,20 @@ public class UserLoginController {
     return (doc != null && doc.getString("password").equals(jsObj.get("password")));
   }
 
-  @RequestMapping("/GetUserProfile")
+  @RequestMapping("/GetUserProfileName")
   public String getFullName(@RequestBody String usr) { //validate a user against the db
+    JSONObject jsObj = new JSONObject(usr);
+    MongoClient usrMC = DBUtils.getusrMC();
+    MongoDatabase db = usrMC.getDatabase("Users");
+    MongoCollection<Document> dbCollection = db.getCollection("users");
+
+    Document doc = DBUtils.findDoc(dbCollection, "email", jsObj.get("username").toString());
+    
+    return doc.getString("fullname");
+  }
+
+  @RequestMapping("/GetUserProfileBeer")
+  public String getFavBeers(@RequestBody String usr) { //validate a user against the db
     System.out.println("Get user data");
     System.out.println(usr);
 
@@ -53,23 +65,17 @@ public class UserLoginController {
 
     Document doc = DBUtils.findDoc(dbCollection, "email", jsObj.get("username").toString());
 
+    
     JSONObject responseJSON = new JSONObject();
     String BEERSFIELDNAME = "beers";
     String prefString = doc.getString("preferences");
     String beerString = prefString.substring(prefString.indexOf(BEERSFIELDNAME) + 5);
 
-    
-  
     MongoDatabase beersDB = usrMC.getDatabase("BeerDB");
     MongoCollection<Document> beerDBCollection = beersDB.getCollection("beers");
     Document beerdoc = beerDBCollection.find().first();
 
-    // String myjson = beerdoc.toJson();
-    // JSONObject j = (JSONObject) JSON.parse(myjson);
-    // System.out.println(j.getJSONObject("beers"));
-
     String beerDBSTR = beerdoc.get("beers").toString();
-    System.out.println(beerDBSTR + "\n\n");
 
     HashMap<String, String> nameToImgMap = new HashMap<String, String>();
     List<String> beersnames = new ArrayList<String>();
@@ -90,48 +96,38 @@ public class UserLoginController {
     }
 
     responseJSON.put("fullname", doc.getString("fullname"));
-    
-
-    System.out.println(lines);
-    System.out.println(lines.get(0));
-    System.out.println(lines.get(3));
-
-    
     responseJSON.put(lines.get(0), lines.get(3));
     for (int i = 4; i < lines.size(); i += 4)
     {
       String name = lines.get(i).substring(8, lines.get(i).length() - 1);
       String url = lines.get(i + 3).substring(7, lines.get(i + 3).length() - 2);
-      System.out.println(i + " " + name);
-      System.out.println((i + 3) + " " + url);
+      // System.out.println(i + " " + name);
+      // System.out.println((i + 3) + " " + url);
 
       responseJSON.put(name, url);
     }
 
-    System.out.println(responseJSON);
 
     List<String> beersToAdd = new ArrayList<String>();
     for (String beer : beerString.split(","))
     {
       if (beer.contains("name"))
       {
-        //System.out.println(beer.substring(beer.indexOf("name") + 7, beer.length() - 2));
         beersToAdd.add(beer.substring(beer.indexOf("name") + 7, beer.length() - 2));
       }
         
     }
     String last = beersToAdd.get(beersToAdd.size() - 1);
     beersToAdd.set(beersToAdd.size() - 1, last.substring(0, last.length() - 2));
-    System.out.println(beersToAdd);
 
-    //System.out.println(beerdoc.get("beers"));
+    JSONObject finalJSON = new JSONObject();
+    for (String str : beersToAdd)
+    {
+      finalJSON.put(str, responseJSON.getString(str));
+    }
 
-    
-    
-    // return new ResponseEntity(HttpStatus.OK);
-    System.out.println(doc.getString("fullname"));
-    
-    
-    return doc.getString("fullname");
+    // System.out.println(finalJSON);
+    return finalJSON.toString();
+    // return doc.getString("fullname");
   }
 }
